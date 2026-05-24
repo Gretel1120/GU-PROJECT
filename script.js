47659d4103123e74876393b2dca8cf35627c4536
Gretel1120 <gubedom@miumg.edu.gt>
2026-05-23 21:31:00 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+            $('.slider').bxSlider({
+                mode: 'fade', 
+                captions: true, 
+                auto: true, 
+                pause: 8000,
+                slideWidth: 800,
+                adaptiveHeight: true,
+                pager: true
+            });
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
